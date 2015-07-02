0fb9dee6ff6bade8a92ee36030c31e29a6334fd2
phillc <spyyderz@gmail.com>
2015-07-02 11:35:45 -0400

    customizations

---
+++ personal/css.el
@@ -0,0 +1,5 @@
+(add-hook 'css-mode-hook
+      (lambda ()
+        (setq css-indent-offset 2)))
+
+(setq less-css-lessc-command "~/.nvm/v0.12.0/bin/lessc")
---
+++ personal/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+ '(package-selected-packages
+   (quote
+    (coffee-mode key-chord yaml-mode scss-mode geiser yari inf-ruby ruby-tools json-mode js2-mode rainbow-mode elisp-slime-nav cider clojure-mode rainbow-delimiters company smex ido-ubiquitous flx-ido vkill exec-path-from-shell zop-to-char zenburn-theme volatile-highlights undo-tree smartrep smartparens smart-mode-line projectile ov operate-on-number move-text magit imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region easy-kill discover-my-major diminish diff-hl browse-kill-ring beacon anzu ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
